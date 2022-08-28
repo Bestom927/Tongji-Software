3163fc626d2a95fb813d242e3177dd4d1de66448
Bestom927 <74091654+Bestom927@users.noreply.github.com>
2022-08-28 14:00:25 +0800

    test back end

--- app.py
+++ app.py
@@ -1,35 +1,6 @@
-from os import path
-from pathlib import Path
+from backend import creat_app
 
-from flask import Flask, render_template
-from flask_frozen import Freezer
+app = creat_app()
 
-
-template_folder = path.abspath('./wiki')
-
-app = Flask(__name__, template_folder=template_folder, static_folder='static', static_url_path="/static")
-#app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
-app.config['FREEZER_DESTINATION'] = 'public'
-app.config['FREEZER_RELATIVE_URLS'] = True
-app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
-freezer = Freezer(app)
-
-@app.cli.command()
-def freeze():
-    freezer.freeze()
-
-@app.cli.command()
-def serve():
-    freezer.run()
-
-@app.route('/')
-def index():
-    return render_template('pages/index.html')
-
-@app.route('/<page>')
-def pages(page):
-    return render_template(str(Path('pages') / (page.lower() + '.html')))
-
-# Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(host='0.0.0.0', port=8080)
