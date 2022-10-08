cf8193bf8af0ad039d4852d59c1cff68d2f0bc29
tjcanger <2052717@tongji.edu.cn>
2022-10-08 21:54:58 +0800

    消除pipeline

--- src/pages/Attributions/Attributions.js
+++ src/pages/Attributions/Attributions.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import Image from 'react-bootstrap/Image'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 const Attributions = () => {
     return (
         <div className='attributions-container'>
             <img src={require("../../assets/2-1.png")} alt="attr-page-img1" />
-
-
         <Row>
       <Col md={{offset:1}}>
       <h1 style={{
@@ -27,7 +25,16 @@ const Attributions = () => {
         and each member was very important, making us feel like one big family.</p>
       </Col>
       <Col md={{offset:1}}>
-       <p>a card</p>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
       </Col>
       </Row>
       <br/><br/><br/><br/><br/><br/>
