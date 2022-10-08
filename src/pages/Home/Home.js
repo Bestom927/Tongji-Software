01a88f690ffe7d0691ab5a3425425076653930eb
vegetable-yx <1931801344@qq.com>
2022-10-08 21:39:42 +0800

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
-import { PromotionVideo, GameEducation, Brochure } from '../YX/Yangxin';
-import '../YX/Yangxin.css';
+import React from 'react'
 
 const Home = () => {
     return (
-        <div className='home-container'>
-            Home page here
-            <PromotionVideo /><br /><br />
-            <GameEducation />
-            <Brochure />
+        <div className='home-container'
+            style={{
+                height: 1000,
+                width: 1500,
+                backgroundImage: "url(" + require("../../assets/bg-img.jpeg") + ")",
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover'
+            }}>
+
         </div>
     )
 }
