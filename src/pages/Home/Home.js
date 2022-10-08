f0aae8f34774bade1c9c34800d27863dd2751b45
vegetable-yx <1931801344@qq.com>
2022-10-08 21:48:11 +0800

    解决冲突...

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import { PromotionVideo, GameEducation, Brochure } from '../YX/Yangxin';
 
 const Home = () => {
     return (
-        <div className='home-container'
-             style={{
-                height:1000,
-                width:1500,
-                backgroundImage:"url("+require("../../assets/bg-img.jpeg")+")",
-                backgroundRepeat:'no-repeat',
-                backgroundSize: 'cover'
-             }}>
+        <>
+            <div className='home-container'
+                style={{
+                    height: 1000,
+                    width: 1500,
+                    backgroundImage: "url(" + require("../../assets/bg-img.jpeg") + ")",
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover'
+                }}>
 
-        </div>
+            </div>
+            <PromotionVideo /><br /><br />
+            <GameEducation />
+            <Brochure />
+        </>
     )
 }
 
