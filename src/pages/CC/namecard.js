6c986a880471811772494369fa9918c9dba02844
tjcanger <2052717@tongji.edu.cn>
2022-10-11 00:42:08 +0800

    修改头像src

--- src/pages/CC/namecard.js
+++ src/pages/CC/namecard.js
@@ -5,11 +5,11 @@ function NameCard(props){
     <p style={{fontWeight:"bold" ,fontSize:"17px"}}> Our team composed by 5 people, we were mainly responsed for the whole synthetic biology part, including concept generated, contact partnership, data analysis, and etc. 
       </p>
       <br/>
-      <img src={require("../../assets/bio1.png")} alt="bio1" className="portrait"/>
+      <img src="https://static.igem.wiki/teams/4272/wiki/bio1.png" alt="bio1" className="portrait"/>
       &nbsp;&nbsp;
-      <img src={require("../../assets/bio2.png")} alt="bio2" className="portrait"/>
+      <img src="https://static.igem.wiki/teams/4272/wiki/bio2.png" alt="bio2" className="portrait"/>
       &nbsp;&nbsp;
-      <img src={require("../../assets/bio3.png")} alt="bio3" className="portrait"/>
+      <img src="https://static.igem.wiki/teams/4272/wiki/bio3.png" alt="bio3" className="portrait"/>
       &nbsp;&nbsp; &nbsp;&nbsp;····
         </>
     )
@@ -21,13 +21,13 @@ function NameCard(props){
         Our team composed by 4 people, we were mainly responsed for the software construction, game logic design, wiki support, and etc.
       </p>
       <br/>
-      <img src={require("../../assets/zzh.png")} alt="sof1" className="portrait"/>
+      <img src="https://static.igem.wiki/teams/4272/wiki/zzh.png" alt="sof1" className="portrait"/>
       &nbsp;&nbsp;&nbsp;&nbsp;
-      <img src={require("../../assets/cc.png")} alt="sof2" className="portrait"/>
+      <img src="https://static.igem.wiki/teams/4272/wiki/cc.png" alt="sof2" className="portrait"/>
       &nbsp;&nbsp;&nbsp;&nbsp;
-      <img src={require("../../assets/yx.png")} alt="sof3" className="portrait"/>
+      <img src="https://static.igem.wiki/teams/4272/wiki/yx.png" alt="sof3" className="portrait"/>
       &nbsp;&nbsp;&nbsp;&nbsp;
-      <img src={require("../../assets/yfy.png")} alt="sof4" className="portrait"/>
+      <img src="https://static.igem.wiki/teams/4272/wiki/yfy.png" alt="sof4" className="portrait"/>
        &nbsp;&nbsp;
       </>
     )
@@ -39,7 +39,7 @@ function NameCard(props){
        The last part of our project is art design, mainly responsed for the graphic part, presentation poster, wiki design, game UI design, and etc.
      </p>
       <br/>
-      <img src={require("../../assets/lxc.png")} alt="des" className="portrait"/>
+      <img src="https://static.igem.wiki/teams/4272/wiki/lxc.png" alt="des" className="portrait"/>
        &nbsp;&nbsp;
     </>
     )
