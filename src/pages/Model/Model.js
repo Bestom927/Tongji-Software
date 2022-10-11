1155bed93786fd3d4342a36e29e629856a495ac6
Bestom927 <74091654+Bestom927@users.noreply.github.com>
2022-10-11 20:19:45 +0800

    finish model

--- src/pages/Model/Model.js
+++ src/pages/Model/Model.js
@@ -87,11 +87,6 @@ const Model = () => {
 
 
       <Col md={{offset:1}}>
-      <h2 className='head2' style={{
-            color:"#2e77e5",
-         }}
-        > <img src="https://static.igem.wiki/teams/4272/wiki/star-icon.png"
-          style={{width:"3vw",marginTop:"-1vw"}}alt="model"/>Feature Processing</h2>
       </Col>
       <Col md={{span:10,offset:1}}style={{marginBottom:"2vh"}} >
       <p style={{fontSize:"larger"}}>
@@ -108,7 +103,7 @@ const Model = () => {
 
       </p>
         <img src="https://static.igem.wiki/teams/4272/wiki/model-9.png"
-          style={{width:"40%",marginTop:"-10vh"}}alt="model"/>
+          style={{width:"40%",marginTop:"0"}}alt="model"/>
       </div>
       <br/>
       <p>
@@ -123,8 +118,72 @@ const Model = () => {
 
       </Col>
 
+      <Col md={{span:10,offset:1}}style={{marginBottom:"2vh"}} >
+      <p style={{fontSize:"larger"}}>
+      <b>Protein Annotation<br/></b>
+      </p>
+      
+      <p >
+      To obtain a high confidence Acr set, if a protein has a clear function or a conserved structural domain, we tend to consider that the protein performs other functions as a non-Acr protein. We therefore include the feature of whether a candidate protein is annotated or not in our model. We determine whether a protein in the dataset is annotated by applying PSI-BLAST to CDD and pVOG with an e value cutoff of 10e-4.
+      </p>
+      
+      <br/>
+      <p style={{fontSize:"larger"}}>
+      <b>HTH Downstream<br/></b>
+      </p>
+      
+      <p >
+      Guilt association is one of the common methods for Acr mining. Acr proteins are characterized by small size and are not conserved. Downstream of Acr usually encodes a homolog of an HTH-containing protein called Anti-CRISPR-associated protein (Aca), which is significantly more conserved in the virus than Acrs itself, and this greatly facilitates the detection of Acr proteins. We therefore added the feature of whether the candidate protein downstream encodes an HTH homologue to our model.
+      We defined the following criteria to determine the feature: Whether there is an HTH-domain-containing protein encoded downstream of and adjacent to (within three genes) the Acr candidate within the same directon. We subjected the candidate proteins to a PSI-BLAST search against the PVOG and CDD datasets with an e-value cutoff of 5e-3. This feature was analyzed by the presence of HTH or helix-turn-helix terms in the annotation of the hit protein entries.
+      </p>
+
+         <p>For unknown genomic FASTA format files without gene annotation, we performed gene annotation by Prokka[5] software to generate GB format files for subsequent extraction of protein features and genomic sequence features.
+        <br/><br/>
+        <b>Prokka</b><br/>
+        Whole genome annotation is the process of identifying features of interest in a set of genomic DNA sequences, and labelling them with useful information. Prokka is a software tool to annotate bacterial, archaeal and viral genomes quickly and produce standards-compliant output files.
+        </p>
 
 
+      </Col>
+
+      <Col md={{span:10,offset:1}}style={{marginBottom:"2vh"}} >
+      <p style={{fontSize:"larger"}}>
+      <b><span style={{color:"#2e77e5"}}>3 </span>Genome-based features</b>
+      </p>
+
+         <p>
+         After annotating the proteins, we introduced the concept of "directon", inspired by the literature[6]. The definition of directon in the literature is shown below, where the Acr gene is shown in gold and the surrounding proteins are indicated in blue. One of the characteristics of Acr proteins is that they are often located upstream of an HTH domain-containing gene. The Guilt-by-association approach commonly used in current Acr prediction models is based on the anti-CRISPR-associated protein (Aca), an HTH-containing protein usually encoded downstream of Acrs. Because Aca is significantly more conserved in the virus than Acrs itself, it is more easily detected. So, we can search the genomic neighborhoods encoding Aca homologs for potential Acrs. In terms of feature calculation, we refer to contiguous proteins on the same strand of up to 100 bp between adjacent proteins as a set of directons, and Acrs may fall in a directon along with small, indeterminate proteins.
+         </p>
+        <img src="https://static.igem.wiki/teams/4272/wiki/model-11.png"
+          style={{width:"100%"}}alt="model"/>
+         <p>
+         Based on this concept we calculated four directon-based features, which are: the directon size, the number of proteins in the directon; the average length of proteins in the directon; the average spacer length of proteins in the directon; and the presence or absence of hth within the three genes downstream of the direction, expressed as T/F.
+        <br/>Finally, we obtained the 12-dimensional feature matrix of the proteins, which was used for our construction of the prediction model.
+
+         </p>
+      </Col>
+
+      <Col md={{offset:1}}>
+      <h2 className='head2' style={{
+            color:"#2e77e5",
+         }}
+        > <img src="https://static.igem.wiki/teams/4272/wiki/star-icon.png"
+          style={{width:"3vw",marginTop:"-1vw"}}alt="model"/>Data Pre-processing          </h2>
+      </Col>
+      <Col md={{span:10,offset:1}}style={{marginBottom:"2vh"}} >
+      
+      <p>
+         
+      In total, there were 73 positive and 31,428 negative data from all the genomes obtained. We first preprocessed the data, mainly for processing the negative data to reduce the number. Firstly, we restricted the length between &gt;=30 and &lt;=400, which mainly considered the length characteristics of Acr proteins; then we used cd-hit to de-redundant the data, setting the similarity greater than or equal to 40% as a class, and finally obtained 13725 negative data.
+      </p>
+      <p>
+      <b>cd-hit [7]</b><br/>
+      It is a tool for clustering protein sequences or nucleic acid sequences to remove redundant sequences based on sequence similarity and is generally used to construct non-redundant data sets for subsequent experimental analysis.
+      cd-hit is a greedy incremental clustering method that first sorts the input sequences according to their length and processes them in order from longest to shortest. The longest sequence is automatically classified into the first class and becomes the representative sequence of the first class. And then, the remaining sequences are compared with the representative sequences found before them. They are classified into one of the classes or become the representative sequence of a new cluster according to the sequence similarity. In the default mode, the sequences are compared only with the representative sequences in each cluster (the longest sequence in this class) and not with the other sequences in this class. 
+
+
+      </p>
+      </Col>
 
 
 
